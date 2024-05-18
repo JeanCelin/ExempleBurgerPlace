c07b29ed27241ad761c519863d601c8a6f35c3f4
Jean Celin <jean_estudos@hotmail.com>
2024-05-18 09:44:12 -0300

    created navegation bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import "./App.css";
-import BurgerLogo from "./img/buger-logo.jpg";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <img src={BurgerLogo} alt="logo" title="logo"></img>
-      <a href="https://br.freepik.com/vetores-gratis/vintage-fundo-com-hamburguer-no-estilo-minimalista_1133886.htm#fromView=search&page=1&position=1&uuid=6f849cc9-1b78-4f5f-ace1-fb9ed0952c35">
-        Imagem de freepik
-      </a>
-      git init
+    <div className="container">
+      <Navbar />
     </div>
   );
 }
