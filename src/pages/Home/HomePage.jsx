9c1cfb7fea22481923863511da39c1bfb7cd81f7
Jean Celin <jean_estudos@hotmail.com>
2024-06-03 18:01:49 -0300

    removed Home component

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,14 +1,40 @@
-import style from "./HomePage.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import Home from "./Home";
+import style from "./HomePage.module.css";
 import Features from "./Features";
 import GalleryHome from "./GalleryHome";
 import Menu from "../../components/layout/Menu";
 import Footer from "../../components/layout/Footer";
+import ButtonHome from "../../components/buttons/ButtonHome";
+import Navbar from "../../components/layout/Navbar";
+import "../../components/styles/globalStyles.css";
+
+AOS.init();
 export default function HomePage() {
   return (
-    <div className={style.homeContainer}>
-      <Home />
+    <div>
+      <div className={style.homeContainer}>
+        <div className={style.background}></div>
+        <Navbar />
+        <div className={style.homeContent}>
+          <h1
+            className={style.homeTitle}
+            data-aos="fade-right"
+            data-aos-delay="50"
+            data-aos-duration="3000">
+            The Burger
+          </h1>
+          <p
+            className={style.homeText}
+            data-aos="fade-left"
+            data-aos-delay="300">
+            Located in the nearest shopping mall.
+            <br /> We will be happy to serve you!
+          </p>
+          <ButtonHome />
+        </div>
+      </div>
       <Features />
       <GalleryHome />
       <Menu />
